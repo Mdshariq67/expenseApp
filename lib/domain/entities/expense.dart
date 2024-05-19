fc562e2b8f39177b1443128675a599effd169a50
Md Sharique Minhaz <mdshariqueminhaz@gmail.com>
2024-05-19 16:15:01 +0530

    Project added

--- lib/domain/entities/expense.dart
+++ lib/domain/entities/expense.dart
@@ -5,10 +5,28 @@ class Expense {
   final String description;
   final String type;
 
-  Expense(
-      {required this.id,
-      required this.amount,
-      required this.date,
-      required this.description,
-      required this.type});
+  Expense({
+    required this.id,
+    required this.amount,
+    required this.date,
+    required this.description,
+    required this.type,
+  });
+
+  // Method to create a copy of Expense with updated fields
+  Expense copyWith({
+    int? id,
+    double? amount,
+    DateTime? date,
+    String? description,
+    String? type,
+  }) {
+    return Expense(
+      id: id ?? this.id,
+      amount: amount ?? this.amount,
+      date: date ?? this.date,
+      description: description ?? this.description,
+      type: type ?? this.type,
+    );
+  }
 }
